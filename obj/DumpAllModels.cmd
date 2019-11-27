14229f1173d196647852fbe33c00523c476c2fed
Daraan <github.blurry@9ox.net>
2019-11-27 22:35:20 +0100

    Added Array fix

--- obj/DumpAllModels.cmd
+++ obj/DumpAllModels.cmd
@@ -1,11 +1,11 @@
 ::Designed to dump all your .bin files from this folder to this folder, 
-::But for convenience you could also choose a sub folder like (.\obj\*.bin) or a whole different file type.
+::But for convenience you could also choose another sub folder like (.\obj\*.bin) or a whole different file type.
 SET Filter=(.\*.bin)
 
 ::Choose your output file name:
 SET Outputname=DumpModels.txt
 
-::Dump into one line? Without extensions
+::Dump into one line? Without extensions.
 ::This is needed if want to use them with the DDumpModels script. They need to be on one long array. [Model1,Model2,...]
 ::Set to something else than YES if you want a long list
 SET OneLine=NO
@@ -19,9 +19,12 @@ SET WithExtenstions=NO
 @echo off
 setlocal enableDelayedExpansion
 
+
+
+
 ( for %%a in %Filter% do (
 	if %OneLine%==YES ( 
-		 <nul set /p=%%~na,
+		 <nul set /p=""%%~na","
 	) else ( 
 		if %WithExtenstions%==YES ( 
 		@echo %%~nxa
